f1908bd773d68b49576e3b93b3d581b0219a5910
Ticktock <smu@ticktock.local>
2018-08-06 13:16:30 +0800

    installed client packages and added nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { NavItem, Navbar } from 'react-materialize';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar>
+          <NavItem href='#'><h5>About Me</h5></NavItem>
+          <NavItem href='#'><h5>My To Dos</h5></NavItem>
+        </Navbar>
+
+        <Header />
       </div>
     );
   }
