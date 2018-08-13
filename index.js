7472008c0611ebbfa6349a94b60528d0457b1739
Ticktock <smu@ticktock.local>
2018-08-13 18:51:20 +0800

    updated v1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose'); //orm
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 const app = express();
@@ -25,6 +25,14 @@ mongoose
 //Use routes
 
 app.use('/api/items', items)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 const port = process.env.PORT || 5000;
 
